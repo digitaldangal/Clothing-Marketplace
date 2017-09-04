aa7cbf8048257eaaeb5a3ebd336f0c3cbbbce4bb
Amidou Kante <kamdiou@gmail.com>
2017-09-04 15:28:25 -0400

    added profile

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from '../config/firebase';
 
 class Login extends Component{
     constructor(props){
@@ -12,6 +13,16 @@ class Login extends Component{
         e.preventDefault();
         console.log('Logging In')
         console.log(this.state.email, this.state.pass)
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(err=>{
+            // Handle Errors here.
+            var errorCode = err.code;
+            var errorMessage = err.message;
+            console.log(errorCode, errorMessage)
+        });
     }
     handleChange=(e)=>{
         e.preventDefault();
@@ -25,8 +36,11 @@ class Login extends Component{
                 <form onSubmit={this.handleSubmit}>
                     <input type="text" placeholder="email" name="email" onChange={this.handleChange}/>
                     <input type="password" placeholder="password" name="password" onChange={this.handleChange}/>
+                    <input type="submit" value="submit"/>
                 </form>
             </div>
         )
     }
 }
+
+export default Login;
