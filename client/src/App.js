ea40df98984c953c088c37983273781e9fed587e
Amidou Kante <kamdiou@gmail.com>
2017-09-04 15:07:30 -0400

    login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import{BrowserRouter as Router, Redirect, Switch, Routes, Link} from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import{BrowserRouter as Router, Redirect, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  handleSubmit=(e)=>{
+    e.preventDefault();
+  }
   render() {
     return (
       <Router>
         <div className="App">
-          
+          <header>
+              <div className='wrapper'>
+                <h1>Copped App</h1>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+              </div>
+          </header>
+          <div className="app-body">
+            <Switch>
+              <Route exact path="/login" render={() => <Login submit={this.handleLoginSubmit} />} />
+              <Route exact path="/register" render={() => <Register submit={this.handleRegisterSubmit} />}/>
+            </Switch>
+          </div>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
         </div>
       </Router>
     );
