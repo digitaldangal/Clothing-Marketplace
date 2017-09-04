41b695b5d0d60a77513b4c2a606f6a347ff0f3d3
Amidou Kante <kamdiou@gmail.com>
2017-09-04 14:40:57 -0400

    Added firebase files

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,4 +1,5 @@
- // Initialize Firebase
+import * as firebase from 'firebase';
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyC76udYV-SsFgyCI4mUSEMi1RXQpMG6VyI",
     authDomain: "copped-9a558.firebaseapp.com",
@@ -6,7 +7,7 @@
     projectId: "copped-9a558",
     storageBucket: "copped-9a558.appspot.com",
     messagingSenderId: "1094268024837"
-  };
-  firebase.initializeApp(config);
-
-  export default firebase;
+	};
+	
+firebase.initializeApp(config);
+export default firebase;
