72fa91fa6936bb080697cb1ad2a330a5cb63a822
Amidou Kante <kamdiou@gmail.com>
2017-09-04 14:38:10 -0400

    Added firebase

---
+++ client/src/config/firebase.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC76udYV-SsFgyCI4mUSEMi1RXQpMG6VyI",
+    authDomain: "copped-9a558.firebaseapp.com",
+    databaseURL: "https://copped-9a558.firebaseio.com",
+    projectId: "copped-9a558",
+    storageBucket: "copped-9a558.appspot.com",
+    messagingSenderId: "1094268024837"
+  };
+  firebase.initializeApp(config);
+
+  export default firebase;
