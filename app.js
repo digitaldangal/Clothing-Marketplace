d779aa99214da0093e2f177c170d48a707e1ffb8
Amidou Kante <kamdiou@gmail.com>
2017-10-05 12:53:27 -0400

    app config

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
-const port = 8080;
-
+const port = process.env.PORT || 8080;
 require('dotenv').config();
 
+mongoose.connect();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, ()=>{
-    console.log(`On port ${port}`)
-    console.log(process.env.mongodb_uri)
+    console.log(`On port ${port}`);
+    console.log(process.env.mongodb_uri);
 }) 
