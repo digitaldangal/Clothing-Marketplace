895cef05a4f60f708a5a9160f3c8dac9439e749c
Amidou Kante <kamdiou@gmail.com>
2017-10-11 18:15:13 -0400

    add footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    const style = {
+        marginTop: "1em",
+        marginBottom: "1em"
+    }
+    return(
+        <footer style={style} >
+            <h4>Amidou Kante &copy; 2017 </h4>
+        </footer>
+    )
+}
+export default Footer;
