7a09c054b65c4c4dbb28823837cac2684972dced
Amidou Kante <kamdiou@gmail.com>
2017-10-11 02:11:37 -0400

    added productupload component

---
+++ client/src/components/ProductUpload.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ProductUpload extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Product Upload Page</h1>
+            </div>
+        )
+    }
+}
+
+export default ProductUpload;
