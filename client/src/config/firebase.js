26c1cdf23d06a65f00f44186d25efaa9e2759d8b
Amidou Kante <kamdiou@gmail.com>
2017-10-12 00:09:25 -0400

    gitignore

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -5,7 +5,7 @@ import * as firebase from 'firebase';
     authDomain: "copped-9a558.firebaseapp.com",
     databaseURL: "https://copped-9a558.firebaseio.com",
     projectId: "copped-9a558",
-    storageBucket: "copped-9a558.appspot.com",
+    storageBucket: "gs://copped-9a558.appspot.com/",
     messagingSenderId: "1094268024837"
 	};
 	
