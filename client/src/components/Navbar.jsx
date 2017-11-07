ca292b5e8b75f09c499ba8d3c13740d99224de57
Amidou Kante <kamdiou@gmail.com>
2017-11-07 13:59:34 -0500

    added dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,6 +11,22 @@ class Navbar extends Component {
         }
     }
 
+    authUser=()=>{
+        return(
+            <div class="ui compact menu">
+                <div class="ui simple dropdown item">
+                    Dropdown
+                    <i class="dropdown icon"></i>
+                    <div class="menu">
+                    <div class="item">Choice 1</div>
+                    <div class="item">Choice 2</div>
+                    <div class="item">Choice 3</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     handleLogOut=()=>{
         if(window.confirm("Do you want to log out?")){
             firebase.auth().signOut()
