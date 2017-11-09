7230c46e1d4eae4e96d98cf3bb3a0478cd0a1857
Amidou Kante <kamdiou@gmail.com>
2017-11-09 16:27:31 -0500

    render page based on brand submission

--- client/src/components/BrandForm.jsx
+++ client/src/components/BrandForm.jsx
@@ -93,10 +93,65 @@ class BrandForm extends Component{
                 brandRef.get().then((res)=>{
                     if(res.data().approved){
                         // Brand is approved
+                    }else{
+                        var errorMessage = `Your brand has not been approved yet! \n Contact us if this problem persists : `;
+                        var formError = document.getElementById("brand-error");
+                        formError.innerHTML = (`<div class="ui message"> <div class="header">We had some issues</div><ul class="list"><li>${errorMessage}</li></ul></div>`) 
                     }
                 })
             }else{
-                // Render brand form
+                return(
+                    <div>
+                        <h1 className="page-title">Create a Brand</h1>
+                        <h3>Brands must first be approved before you are allowed to post</h3>
+                        <form onSubmit={this.handleSubmit} className="ui equal width form">
+                            <div id="form-error"></div>
+                            <div className="two fields">
+                                <div className="field">
+                                    <div className="ui labeled input">
+                                        <div className="ui label">
+                                            Brand Name
+                                        </div>
+                                        <input required="true" name="name" type="text" placeholder="Brand Name" onChange={(e)=>this.handleChange(e)}/>
+                                    </div>
+                                </div>
+                                <div className="field">
+                                    <div className="ui labeled input">
+                                        <div className="ui label">
+                                            Inventory
+                                        </div>
+                                        <input required="true" name="inventory_size" type="number" placeholder="Inventory Count" onChange={(e)=>this.handleChange(e)}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="three field">
+                                <div className="field">
+                                    <label>Brand Description</label>
+                                    <textarea required="true" name="description" rows="2" placeholder="Brand Description" onChange={(e)=>this.handleChange(e)}></textarea>        
+                                </div>
+                                <div className="field">
+                                    <label>Shipping Address</label>
+                                    <textarea name="shipping_address" rows="2" placeholder="Where will you ship from?" onChange={(e)=>this.handleChange(e)}></textarea>
+                                </div>
+                                <div className="field">
+                                    <label>Social Media Links</label>
+                                    <textarea name="links" rows="2" placeholder="Separate All Social Media links with a comma" onChange={(e)=>this.handleChange(e)}></textarea>
+                                </div>
+                            </div>
+
+                            <div className="field">
+                                <div className="ui labeled input">
+                                    <div className="ui label">
+                                        Website Url
+                                    </div>
+                                    <input name="website" type="text" placeholder="https://example.com" onChange={(e)=>this.handleChange(e)}/>
+                                </div>
+                            </div>
+                            <button className="ui primary button" type="submit">Save</button>
+                        </form>
+                    </div>
+                )
             }
         }else{
             console.log("not logged in yet")
@@ -105,58 +160,11 @@ class BrandForm extends Component{
     render(){
         const {redirect, currentPage} = this.state;
         return(
-            <div>
+            <section id="brand-form">
                 {redirect ? <Redirect to={currentPage} /> : null}
                 {this.renderPage()}
-                <h1 className="page-title">Create a Brand</h1>
-                <h3>Brands must first be approved before you are allowed to post</h3>
-                <form onSubmit={this.handleSubmit} className="ui equal width form">
-                    <div id="form-error"></div>
-                    <div className="two fields">
-                        <div className="field">
-                            <div className="ui labeled input">
-                                <div className="ui label">
-                                    Brand Name
-                                </div>
-                                <input required="true" name="name" type="text" placeholder="Brand Name" onChange={(e)=>this.handleChange(e)}/>
-                            </div>
-                        </div>
-                        <div className="field">
-                            <div className="ui labeled input">
-                                <div className="ui label">
-                                    Inventory
-                                </div>
-                                <input required="true" name="inventory_size" type="number" placeholder="Inventory Count" onChange={(e)=>this.handleChange(e)}/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="three field">
-                        <div className="field">
-                            <label>Brand Description</label>
-                            <textarea required="true" name="description" rows="2" placeholder="Brand Description" onChange={(e)=>this.handleChange(e)}></textarea>        
-                        </div>
-                        <div className="field">
-                            <label>Shipping Address</label>
-                            <textarea name="shipping_address" rows="2" placeholder="Where will you ship from?" onChange={(e)=>this.handleChange(e)}></textarea>
-                        </div>
-                        <div className="field">
-                            <label>Social Media Links</label>
-                            <textarea name="links" rows="2" placeholder="Separate All Social Media links with a comma" onChange={(e)=>this.handleChange(e)}></textarea>
-                        </div>
-                    </div>
-
-                    <div className="field">
-                        <div className="ui labeled input">
-                            <div className="ui label">
-                                Website Url
-                            </div>
-                            <input name="website" type="text" placeholder="https://example.com" onChange={(e)=>this.handleChange(e)}/>
-                        </div>
-                    </div>
-                    <button className="ui primary button" type="submit">Save</button>
-                </form>
-            </div>
+                <div id="brand-error"></div>
+            </section>
         )
     }
 }
