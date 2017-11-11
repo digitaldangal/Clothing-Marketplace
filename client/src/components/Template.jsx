0521bfaead4098e35c66fa3b7458b2ac5a695976
Amidou Kante <kamdiou@gmail.com>
2017-11-10 20:43:49 -0500

    component template

--- client/src/components/Template.jsx
+++ client/src/components/Template.jsx
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 
-class ApprovedBrand extends Component {
+class Template extends Component {
     constructor(props){
         super(props);
         this.state = {
-            
+
         }
     }
-    
-    //Check status of approved brand
-
 
     render(){
         return(
             <div>
-                <h1 className="page-title">Name of Brand</h1>
+                
             </div>
         )
     }
 }
 
-export default ApprovedBrand;
+export default Template
