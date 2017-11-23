8fdc19e23ca67018d7dc019906e5278e69bc1659
Amidou Kante <kamdiou@gmail.com>
2017-11-23 01:29:44 -0500

    retrieve articles from firebase and store in state

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -9,12 +9,21 @@ class Article extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            articleData: false,
         }
     }
 
     componentWillMount() {
-        
+        let articleFeed = db.collection("articles").orderBy("created");
+        let tempArticleData = {}
+
+        articleFeed.limit(15).get().then((res)=>{
+            res.forEach((article)=>{
+                console.log(article.data())
+                tempArticleData[article.data().title] = article.data()
+                this.setState({articleData: tempArticleData})
+            })
+        }).catch(err=>console.log(err))
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -22,15 +31,15 @@ class Article extends Component {
     }
 
     rendePage(){
-        if(){
+        // if(){
 
-        }else{
-            return(
-                <div className="ui active inverted dimmer">
-                    <div className="ui indeterminate text loader">Preparing Files</div>
-                </div>
-            )
-        }
+        // }else{
+        //     return(
+        //         <div className="ui active inverted dimmer">
+        //             <div className="ui indeterminate text loader">Preparing Files</div>
+        //         </div>
+        //     )
+        // }
     }
 
     render(){
@@ -38,7 +47,9 @@ class Article extends Component {
         return(
             <div>
                 {redirect ? <Redirect to={currentPage} /> : null}
-                {this.rendePage()}
+                <section className="article-feed">
+                    {this.rendePage()}
+                </section>
             </div>
         )
     }
