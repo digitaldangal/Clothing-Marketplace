6b7c832634e75416e00c37fa2ed340261bcab174
Amidou Kante <kamdiou@gmail.com>
2017-11-23 00:58:21 -0500

    started component to list all articles

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom';
+import firebase from '../config/firebase';
+
+// Initialize Cloud Firestore through firebase
+var db = firebase.firestore();
 
 class Article extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentWillMount() {
+        
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        
+    }
+
+    rendePage(){
+        if(){
+
+        }else{
+            return(
+                <div className="ui active inverted dimmer">
+                    <div className="ui indeterminate text loader">Preparing Files</div>
+                </div>
+            )
+        }
+    }
 
     render(){
+        const {redirect, currentPage} = this.state;
         return(
-            <h1>Article Section</h1>
+            <div>
+                {redirect ? <Redirect to={currentPage} /> : null}
+                {this.rendePage()}
+            </div>
         )
     }
 }
