c2470124a8532a683c3328975f273507575385e2
Amidou Kante <kamdiou@gmail.com>
2017-11-23 02:51:26 -0500

    added featured article to article feed

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Redirect} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import firebase from '../config/firebase';
 
 // Initialize Cloud Firestore through firebase
@@ -19,29 +19,35 @@ class Article extends Component {
         let tempArticleData = {}
         let featuredArticleData = {}
 
-        articleFeed.limit(15).get().then((res)=>{
+        articleFeed.limit(1).get().then((res)=>{
             res.forEach((article)=>{
                 return tempArticleData[article.data().title] = article.data()
             })
             this.setState({articleData: tempArticleData})
         }).then(()=>{
             db.collection("articles").doc("article_0").get().then((res)=>{
-                return featuredArticleData = res.data();
+                console.log(res.data())
+                featuredArticleData = res.data();
+                this.setState({featuredArticle: featuredArticleData})
             })
-            this.setState({featuredArticle: featuredArticleData})
         })
         .catch(err=>console.log(err))
     }
 
     rendePage(){
-        if(this.state.articleData !== false){
-            const {articleData} = this.state;
+        if(this.state.articleData !== false && this.state.featuredArticle !== false){
+            const {articleData, featuredArticle} = this.state;
             return(
                 <div className="article-list">
                     <h1 className="ui header title">Latest Articles</h1>
                     <div className="page-container article-list ui container">
                         <div className="featured-article">
-                        
+                            <Link to={`/editorial/${featuredArticle.id}/${featuredArticle.title}`}>
+                                <img src={featuredArticle.screen_image } alt={featuredArticle.title} title={featuredArticle.title}/>
+                                <div className="overlay"></div>
+                                <h2 className="ui header article-title">{featuredArticle.title}</h2>
+                                <h3 className="ui header article-subtitle">{featuredArticle.subtitle}</h3>
+                            </Link>
                         </div>
                         {Object.values(articleData).map((article, i)=>{
                             return(
