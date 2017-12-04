8f2c8663dee5863377c467857b2b397777ad69a4
Amidou Kante <kamdiou@gmail.com>
2017-12-03 20:41:52 -0500

    final changes to product upload lifecycle

--- client/src/options/SizeChoose.jsx
+++ client/src/options/SizeChoose.jsx
@@ -5,40 +5,40 @@ const SizeChoose=(props)=>{
     switch (productDetails.category) {
         case "OUTERWEAR":
         case "TOPS":
-            return(
-                <select required name="size" onChange={(e)=>props.handleChange(e)}>
-                    <option value="">SELECT</option>
-                    {Object.values(productDetails.inventory).map((size, i)=>{
-                    return(
-                        size > 0 ? <option key={i} value={size}>{size}</option> : <option disabled>{size}</option>
-                    )
-                    })}
-                </select>
-            )
+        return(
+            <select required name="size" onChange={(e)=>props.handleChange(e)}>
+                <option value="">SELECT</option>
+                {Object.values(productDetails.inventory).map((size, i)=>{
+                return(
+                    size > 0 ? <option key={i} value={size}>{Object.keys(productDetails.inventory)[i].toUpperCase()}</option> : <option disabled>{Object.keys(productDetails.inventory)[i].toUpperCase()}</option>
+                )
+                })}
+            </select>
+        )
 
         case "BOTTOMS":
-            return(
-                <select required name="size" onChange={(e)=>props.handleChange(e)}>
-                    <option value="">SELECT</option>
-                    {Object.values(productDetails.inventory).map((size, i)=>{
-                    return(
-                        size > 0 ? <option key={i} value={size}>{Object.keys(productDetails.inventory)[i]}</option> : <option disabled>{Object.keys(productDetails.inventory)[i]}</option>
-                    )
-                    })}
-                </select>
-            )
+        return(
+            <select required name="size" onChange={(e)=>props.handleChange(e)}>
+                <option value="">SELECT</option>
+                {Object.values(productDetails.inventory).map((size, i)=>{
+                return(
+                    size > 0 ? <option key={i} value={size}>{Object.keys(productDetails.inventory)[i].toUpperCase()}</option> : <option disabled>{Object.keys(productDetails.inventory)[i].toUpperCase()}</option>
+                )
+                })}
+            </select>
+        )
 
         case "ACCESSORIES":
-            return(
-                <select required name="size" onChange={(e)=>props.handleChange(e)}>
-                    <option value="">SELECT</option>
-                    {Object.values(productDetails.inventory).map((size, i)=>{
-                    return(
-                        size > 0 ? <option key={i} value={size}>{size}</option> : <option disabled>{size}</option>
-                    )
-                    })}
-                </select>
-            )
+        return(
+            <select required name="size" onChange={(e)=>props.handleChange(e)}>
+                <option value="">SELECT</option>
+                {Object.values(productDetails.inventory).map((size, i)=>{
+                return(
+                    size > 0 ? <option key={i} value={size}>{Object.keys(productDetails.inventory)[i].toUpperCase()}</option> : <option disabled>{Object.keys(productDetails.inventory)[i].toUpperCase()}</option>
+                )
+                })}
+            </select>
+        )
     
         default:
             return(
