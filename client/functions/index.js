c000039a4f2d21cfc5cf7c8e26d17cd5cee7699c
Amidou Kante <kamdiou@gmail.com>
2017-12-14 14:38:37 -0500

    updated index js

--- client/functions/index.js
+++ client/functions/index.js
@@ -43,8 +43,8 @@ exports.pay = functions.https.onRequest((req, res) => {
       payment_method: 'paypal'
     },
     redirect_urls: {
-      return_url: `http://localhost:5000/profile/process`,
-      cancel_url: `http://localhost:5000/profile`
+      return_url: `https://streetwearboutiques.com/profile/process`,
+      cancel_url: `https://streetwearboutiques.com/profile`
     },
     transactions: [{
       "amount": {
@@ -122,7 +122,7 @@ exports.process = functions.https.onRequest((req, res) => {
   paypal.payment.execute(paymentId, payerId, (error, payment) => {
     if (error) {
       console.error(error);
-      res.redirect(`http://localhost:5000/profile/error`); // replace with your url page error
+      res.redirect(`https://streetwearboutiques.com/profile/error`); // replace with your url page error
     } else {
       if (payment.state === 'approved') {
         console.info('payment completed successfully, description: ', payment.transactions[0].description);
@@ -144,7 +144,7 @@ exports.process = functions.https.onRequest((req, res) => {
             'payer': payment.payer
           }
         }).then(r => console.info('promise: ', r)).catch(err=>console.log(err));
-        res.redirect(`http://localhost:5000/profile/process`); // replace with your url, page success
+        res.redirect(`https://streetwearboutiques.com/profile/process`); // replace with your url, page success
       } else {
         console.warn('payment.state: not approved ?');
         // replace debug url
