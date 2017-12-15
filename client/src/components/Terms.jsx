943df87c3c79585d4b09a1ec8ce965209b51e190
Amidou Kante <kamdiou@gmail.com>
2017-12-15 16:56:07 -0500

    added terms

---
+++ client/src/components/Terms.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import Footer from './Footer';
+import {Link} from 'react-router-dom';
+
+const Terms = (props)=> {
+    const paraTitle = {
+        marginBottom: '1em',
+        textAlign: 'center',
+        marginTop: '1em',
+    }
+    return(
+        <section id="about">
+            <div className="about-page ui container">
+                <h1 className="ui header">Discover Streetwear Boutiques</h1>
+                <h3 className="ui header">Expand Your Wardrobe</h3>
+                <div className="page-container ui container">
+                    <div className="ui divided items container terms">
+                        <div className="item paragraph">
+                            <p className="text">
+                            Streetwear Boutiques was founded to bridge the gap between new & emerging clothing brands, and fashion lovers alike. Streetwear Boutiques provides a centralized, exclusive, and curated selection of various labels, with the goal of building a diverse community that connects directly to the user. As more clothing brands sign up, the community and user base increases. Streetwear Boutiques is the first platform to provide a home for new, innovative, unique, and emerging brands. Streetwear Boutiques has a fair and transparent business model that maximizes revenue Designers can earn from their clothing labels. Streetwear Boutiques has no monthly fees and only takes a commission on each transaction. 
+                            Designers are allowed to register as a brand, upload and post clothing that they created. Hosting, distribution, payment processing, and marketing are taken care of by Streetwear Boutiques. This way Designers can focus on improving their brand and expanding their reach. 
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Terms of Service</h1>
+                            By using Streetwear Boutiques, you are agreeing to be bound by these terms of service, all applicable laws, and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing Streetwear Boutiques. These terms reflect the entirety of the agreement between users and Streetwear Boutiques. Streetwear Boutiques can make changes to these terms at any time. Users will be notified via email should these terms change.
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Users and User Conduct</h1>
+                            Users on Streetwear Boutiques must be over the age of 13. Accounts held by users under the age of 13 will be terminated. Users are responsible for safeguarding their accounts, it is advised that users use a strong password and not share their account credentials with others via email or any other forms. Streetwear Boutiques will never email users to ask for their account password. Users are responsible for the proper conduct of their accounts. User accounts can be terminated for the following reasons: abuse and harassment, fraud, impersonation, spam, and intellectual property infringements.
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Users</h1>
+                            Streetwear Boutiques is free for users, to use. They only time they are requested to make a payment is when they are buying from a Designer on the site. Users recognize that Streetwear Boutiques has no control or input over the quality & designs of clothing by Designers on Streetwear Boutiques. Streetwear Boutiques does grant refunds on a case by case basis.
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Designers</h1>
+                            To become a recognized Designer on Streetwear Boutiques, you must sign up as a user first and then register as a brand. Registering as a brand requires that you provide your full legal name, physical address, and PayPal email for verification with our payment processor, PayPal. You can find more information about this in our Privacy Policy. Designers who have not been verified as labels are prohibited from posting clothing on Streetwear Boutiques. Streetwear Boutiques takes a 0% commission on all processed transactions. The only fees are those that PayPal charge which is 2.9% + $0.30. Designers are responsible for reporting and paying any applicable taxes in connection with their participation in Streetwear Boutiques. Designers own the rights to all clothing and information posted on Streetwear Boutiques.
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Streetwear Boutiques Content and Services</h1>
+                            We may change, terminate, or restrict access to any aspect of the service, at any time, without notice. Streetwear Boutiques reserves the right to remove the content of and terminate accounts that publish content that violates trademark or copyright, includes harassment or hate speech towards an individual or a group of individuals, or engages in unlawful conduct. Streetwear Boutiques is provided “as is” and without warranty of any kind. Payment processing services for Designers on Streetwear Boutiques are provided by PayPal and are subject to the PayPal Marketplace Connected Account Agreement. By agreeing to these or continuing to operate as a Designer on Streetwear Boutiques, you agree to be bound by the PayPal Services Agreement, as the same may be modified by PayPal from time to time. As a condition of Streetwear Boutiques enabling payment processing services through PayPal, you agree to provide Streetwear Boutiques accurate and complete information about you and your business, and you authorize Streetwear Boutiques to share it and transaction information related to your use of the payment processing services provided by PayPal.
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Security and Privacy</h1>
+                            By using Streetwear Boutiques, you agree that KanteLabs can collect and use information as detailed in the Privacy Policy. If you find a security vulnerability, please contact us.
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Privacy Policy</h1>
+                            This Privacy Policy describes how and when we collect, use, and share your information across Streetwear Boutiques. When using Streetwear Boutiques, you consent to the collection, transfer, storage, disclosure, and use of your information, including any information that is deemed sensitive under applicable law, as described in this Privacy Policy.
+                            <br /><br />
+                            What personal information do we collect from users on Streetwear Boutiques?
+                            When registering on our website, users will be asked to provide an email address, username, password, and full legal name.
+                            <br /><br />
+                            When registering as a Designer, users will be asked to provide a brand name, PayPal email, description, inventory total, shipping address, social media links, and website. 
+                            <br /><br />
+                            When do we collect information?
+                            This information is collected when users register on our site via the signup form or register as a brand via the brand registration form.
+                            <br /><br />
+                            How can users modify their data on Streetwear Boutiques?
+                            Users can modify certain data on Streetwear Boutiques using the account and profile settings page.
+                            <br /><br />
+                            How do we protect user information?
+                            User information is contained behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. In addition, all sensitive/credit information supplied is encrypted via Secure Socket Layer (SSL) technology. All transactions are processed through a payments gateway provider and are not stored or processed on our servers.
+                            <br /><br />
+                            Do we share user data with third parties?
+                            We do not sell, trade, or otherwise transfer to outside parties user data except to vendors and services (e.g. our payments gateway, PayPal) working on our behalf.
+                            <br /><br />
+                            How do we communicate with users about changes to the Privacy Policy?
+                            Users will receive an email should the Privacy Policy be changed.
+                            <br /><br />
+                            How do we communicate with users about data breaches?
+                            In accordance with Fair Information Practices, we notify users within 7 business days should we become aware of any data breaches that occur.
+                            <br /><br />
+                            Why do we collect user email addresses?
+                            We collect user emails to send information, respond to inquiries, and/or other requests or questions and to process orders and to send information and updates pertaining to orders. In accordance with the CAN-SPAM ACT, users can email us and we will promptly remove them from all correspondences.
+                            <br /><br />
+                            How can users contact us about this Privacy Policy?
+                            Users can send us an email.
+                            </p>
+                        </div>
+                        <div className="item paragraph">
+                            <p className="text">
+                            <h1 className="ui header title" style={paraTitle}>Frequently Asked Questions</h1>
+                            How much does the Streetwear Boutiques cost?
+                            $0.00USD.   
+                            <br /><br />
+                            How is this different from SSENSE/GRAILED/Shopify?
+                            SSENSE is only limited to high end and already establish clothing brands like Gucci and Versace. We provide a home for new brands to be able to establish themselves. 
+                            Grailed is limited to reselling article of clothing from brands that are commonly listed on websites like SSENSE and Barneys. Grailed wasn't really made for people to sell clothing that they created. 
+                            Shopify charges users monthly fees on top of fees for transactions, while Streetwear Boutiques doesn't charge for anything. 
+                            <br /><br />
+                            How can I contact Streetwear Boutiques?
+                            You can send an email to streetwearboutiques@gmail.com
+                            <br /><br />
+                            Who is behind Streetwear Boutiques?
+                            Streetwear Boutiques is built and ran by Amidou Kante. Amidou has been writing software since he was 12-years old and is passionate about building creative, engaging and minimalist products that have a meaningful impact.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </section>
+    )
+}
+
+export default Terms;
