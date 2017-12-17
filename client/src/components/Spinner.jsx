330db45e9eee3a777b4a224a449f5590f8592163
Amidou Kante <kamdiou@gmail.com>
2017-12-17 17:49:45 -0500

    Created a spinner component

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+    return(
+        <div className="ui active inverted dimmer">
+            <div className="ui indeterminate text loader"></div>
+        </div>
+    )
+}
+
+export default Spinner;
