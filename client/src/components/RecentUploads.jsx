61cca4d158bb0ce45f48aa6c5ed0e88867f18665
Amidou Kante <kamdiou@gmail.com>
2017-12-20 13:12:21 -0500

    can view and navigate to recently uploaded

--- client/src/components/RecentUploads.jsx
+++ client/src/components/RecentUploads.jsx
@@ -28,27 +28,37 @@ class RecentUploads extends Component {
 
     renderPage=()=>{
         const {productData} = this.state;
+        const cardLayout = {
+            "display": "flex",
+            "flexFlow": "row wrap",
+            "justifyContent": "center",
+            "marginTop": "1em"
+        }
+        const singleCard = {
+            "margin": "0em 1.5em 1em",
+            "width": "auto"
+        }
         return(
-            <div className="ui link cards recent-upload-container">
+            <div className="ui link cards recent-upload-container" style={cardLayout}>
                 {Object.values(productData).map((product, i)=>{
                     const style = {
                         "backgroundImage": `url(${product.main_image}`,
-                        "background-size": "cover",
+                        "backgroundSize": "cover",
                         "width": "300px",
                         "height": "300px",
-                        "background-position": "center",
-                        "background-repeat": "no-repeat"
+                        "backgroundPosition": "center",
+                        "backgroundRepeat": "no-repeat"
                     }
                     return(
-                        <div className="card" key={i}>
+                        <div className="card" key={i} style={singleCard}>
                             <Link to={`/designers/${product.designer}/${product.clothing_label.id}/${product.title}/${product.id}`}>
                                 <div className="imageHolder" style={style}></div>
                             </Link>    
                                 <div className="content">
-                                    <div className="header">{product.title}</div>
+                                <div className="header">{product.title}</div>
                                         {product.inventory_total === 0 ? (<p id="soldout">SOLD OUT</p>) : null}
                                     <div className="description links">
-                                        {product.title}
+                                        {product.designer}
                                     </div>
                                     <div className="meta links">
                                         <Link to={`/search/products/${product.category.toLowerCase()}`}>{product.category}</Link>
@@ -65,7 +75,7 @@ class RecentUploads extends Component {
     render(){
         return(
             <div id="recent-uploads">
-                <h2 className="ui header">Recently Uploaded</h2>
+                <h2 className="ui header" style={{marginTop: "1em"}}>Recently Uploaded</h2>
                 {this.state.productDataLoaded ? this.renderPage() : null}
             </div>
         )
