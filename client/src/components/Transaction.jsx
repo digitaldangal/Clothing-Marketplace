1fd3d24b521c7252b71f6411353eb238e30dd42f
Amidou Kante <kamdiou@gmail.com>
2017-12-24 00:48:40 -0500

    can display transaction values by date

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Table } from 'semantic-ui-react';
 
 const Transaction = (props) => {
+    const {transactionData} = props;
     return(
         <div className="page-container">
             <Table singleLine>
@@ -11,8 +12,18 @@ const Transaction = (props) => {
                         <Table.HeaderCell>Order Date</Table.HeaderCell>
                         <Table.HeaderCell>Total</Table.HeaderCell>
                         <Table.HeaderCell>Status</Table.HeaderCell>
+                        <Table.HeaderCell>Options</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
+                <Table.Body>
+                    {Object.values(transactionData).map((transaction, i)=>{
+                        return(
+                            <Table.Row key={i}>
+                                <Table.Cell>{new Date(transaction.date).toLocaleDateString()}</Table.Cell>
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
             </Table>
         </div>
     )
