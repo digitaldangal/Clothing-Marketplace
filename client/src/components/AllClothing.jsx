2a4ab710b6e1a3be72ca29a57f2625835388f79a
Amidou Kante <kamdiou@gmail.com>
2017-12-29 13:24:53 -0500

    Created component to hold single clothing

--- client/src/components/AllClothing.jsx
+++ client/src/components/AllClothing.jsx
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import {Redirect, Link} from 'react-router-dom';
 
 class AllClothing extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            redirect: false,
+            currentPage: '',
+            clothingData: false,
+            clothingDataLoaded: false
+        }
+    }
 
     render(){
         const {redirect, currentPage} = this.state;
         return(
             <section id="all-clothing">
                 {redirect ? <Redirect to={currentPage} /> : null}
-                {this.renderPage()}
+                <ListOFBrands />
+                <Clothes />
             </section>
         )
     }
